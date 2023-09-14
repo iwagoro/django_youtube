e568ebf72871ce85adfbefe8bffdac6ffbc862bb
rockwell <iwagoro733@gmail.com>
2023-09-14 14:47:34 +0900

    動画の詳細ページ、コメント投稿フォームを作成しました

--- myyoutube/myyoutube/urls.py
+++ myyoutube/myyoutube/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home',views.home,name='home'),
-    path('upload',views.upload,name='upload'),
+    path('upload',views.upload,name='upload'),\
+    path('video/<int:pk>',views.video_detail,name='video_detail'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myyoutube/youtube/views.py
+++ myyoutube/youtube/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Video,Comment
 from .forms import UploadForm
+from .forms import CommentForm
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.http import HttpResponse
+
 
 
 def home(request):
@@ -27,3 +30,21 @@ def upload(request):
         form = UploadForm()
     return render(request, 'youtube/upload.html', {'form': form})
 
+
+def video_detail(request,pk):
+    selectedVideo = get_object_or_404(Video,pk=pk)
+    videos = Video.objects.all()
+    comments = Comment.objects.filter(video=selectedVideo)
+    if request.method == 'POST':
+        print("post")
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            print("form is valid")
+            comment = form.save(commit=False)
+            comment.video = selectedVideo
+            comment.published_at = timezone.now()
+            comment.save()
+            return redirect('video_detail',pk=pk)
+    else:
+        form = CommentForm()
+    return render(request,'youtube/video_detail.html',{'form':form,'selectedVideo':selectedVideo,'videos':videos,'comments':comments})
