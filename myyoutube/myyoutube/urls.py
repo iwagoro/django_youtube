e6a07e862fe9e04603ae276ac0656d2f1a139800
rockwell <iwagoro733@gmail.com>
2023-09-14 14:58:03 +0900

    動画の詳細ページ、コメント投稿フォームを作成しました

--- myyoutube/myyoutube/urls.py
+++ myyoutube/myyoutube/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home',views.home,name='home'),
-    path('upload',views.upload,name='upload'),\
+    path('upload',views.upload,name='upload'),
     path('video/<int:pk>',views.video_detail,name='video_detail'),
 ]
 if settings.DEBUG:
